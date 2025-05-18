5f2c2acdef377d9ff56ed77a154f9a2226ad31b0
Moisés Santiago Pineda Torres <moises.pineda@epn.edu.ec>
2025-05-18 11:24:52 -0500

    Actualizacion de informacion

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -85,24 +85,21 @@ const siteConfig: SiteConfig = {
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'Github',
+            href: 'https://github.com/SantiagoTmg'
         },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
+
         {
             text: 'X/Twitter',
             href: 'https://twitter.com/'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hi there & welcome to my space on the web!',
+        text: "My name is Moisés, I am currently pursuing a degree in Computer Science Engineering. My areas of interest include programming, data structures, algorithms, artificial intelligence and cybersecurity. In my free time, I enjoy reading books, playing video games, and exploring new technologies",
         image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            src: '/no_robot.png',
+            alt: 'A cat with a robot head',
         },
         actions: [
             {
